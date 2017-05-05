a9c00d3ff55d847328950753502d56e50c7a0c3c
gofey <rgliguohui@163.com>
2017-05-05 10:12:30 +0800

    Initial Commit

---
+++ CustomPhoneAlum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomPhoneAlum
+//
+//  Created by 厉国辉 on 2017/5/5.
+//  Copyright © 2017年 GofeyLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
